2190e8b5676d39868411fb711ff7f96351e932df
Norbert Neudert <norbertneudert@gmail.com>
2019-11-25 11:28:05 +0100

    Added function that creates the IdentityRoles if they weren't already

--- OpenMyGarageApi/OpenMyGarageApi/Data/ApplicationDbContext.cs
+++ OpenMyGarageApi/OpenMyGarageApi/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,6 +12,19 @@ namespace OpenMyGarageApi.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            #region "Seed Data"
+
+            builder.Entity<IdentityRole>().HasData(
+                new { Id = "1", Name = "Admin", NormalizedName = "ADMIN" },
+                new { Id = "2", Name = "User", NormalizedName = "USER" }
+            );
+
+            #endregion
         }
     }
 }
