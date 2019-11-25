de6adc005e1d621b0dd1a89d96750ff22b81ef62
Norbert Neudert <norbertneudert@gmail.com>
2019-11-25 12:10:42 +0100

    Added EntryLog class that represents the table EntryLogs in the database

---
+++ OpenMyGarageApi/OpenMyGarageApi/Models/EntryLog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenMyGarageApi.Models
+{
+    public class EntryLog
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string Plate { get; set; }
+
+        [Required]
+        public DateTime Time { get; set; }
+
+        [Required]
+        public GateAction Outcome { get; set; }
+    }
+}
