1e71040527dadbb3b5f6ed989f9964d8a5642c43
Norbert Neudert <norbertneudert@gmail.com>
2019-11-25 12:39:43 +0100

    Added database access for ValuesController

--- OpenMyGarageApi/OpenMyGarageApi/Controllers/ValuesController.cs
+++ OpenMyGarageApi/OpenMyGarageApi/Controllers/ValuesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OpenMyGarageApi.Data;
 
 namespace OpenMyGarageApi.Controllers
 {
@@ -11,5 +12,11 @@ namespace OpenMyGarageApi.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly ApplicationDbContext db;
+
+        public ValuesController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
     }
 }
