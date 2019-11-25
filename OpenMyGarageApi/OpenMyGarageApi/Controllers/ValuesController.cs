0e2f182eb64e84efce51917ea75c19992d722aa1
Norbert Neudert <norbertneudert@gmail.com>
2019-11-25 12:40:03 +0100

    Added authorization for ValuesController

--- OpenMyGarageApi/OpenMyGarageApi/Controllers/ValuesController.cs
+++ OpenMyGarageApi/OpenMyGarageApi/Controllers/ValuesController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OpenMyGarageApi.Data;
 
 namespace OpenMyGarageApi.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
