45fcca1a572c777aec69286be1b5b6946d0fa51c
Norbert Neudert <norbertneudert@gmail.com>
2019-12-07 20:03:15 +0100

    Added serialization to StoredPlate class

--- OpenMyGarageAndroid/app/src/main/java/com/norbertneudert/openmygarage/database/StoredPlate.kt
+++ OpenMyGarageAndroid/app/src/main/java/com/norbertneudert/openmygarage/database/StoredPlate.kt
@@ -3,18 +3,22 @@ package com.norbertneudert.openmygarage.database
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.squareup.moshi.Json
 
 @Entity(tableName = "stored_plate_table")
 data class StoredPlate (
     @PrimaryKey(autoGenerate = true)
     var plateId: Long = 0L,
 
+    @Json(name = "plate")
     @ColumnInfo(name = "plate")
     var plate: String = "",
 
+    @Json(name = "name")
     @ColumnInfo(name = "name")
     var name: String = "",
 
+    @Json(name = "action")
     @ColumnInfo(name = "outcome")
     var outcome: Int = 1
 )
