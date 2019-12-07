05c8f989d0675a9e83727819d43b80df678fea8b
Norbert Neudert <norbertneudert@gmail.com>
2019-12-07 19:54:36 +0100

    Added more API calls

--- OpenMyGarageAndroid/app/src/main/java/com/norbertneudert/openmygarage/apiservice/OMGApiService.kt
+++ OpenMyGarageAndroid/app/src/main/java/com/norbertneudert/openmygarage/apiservice/OMGApiService.kt
@@ -2,12 +2,16 @@ package com.norbertneudert.openmygarage.apiservice
 
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.norbertneudert.openmygarage.database.EntryLog
+import com.norbertneudert.openmygarage.database.StoredPlate
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.Deferred
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 
 private const val BASE_URL = "https://openmygarageapi.azurewebsites.net/api/values/"
@@ -23,6 +27,15 @@ private val retrofit = Retrofit.Builder()
 interface OMGApiService {
     @GET("entrylogs")
     fun getEntryLogs() : Deferred<List<EntryLog>>
+
+    @GET("storedplates")
+    fun getStoredPlates() : Deferred<List<StoredPlate>>
+
+    @POST("storedplates")
+    fun postStoredPlate(@Body storedPlate: StoredPlate)
+
+    @DELETE("storedplates")
+    fun deleteStoredPlate(@Body storedPlate: StoredPlate)
 }
 
 object OMGApi {
