c9bfbb810dc4d2a53f3127b719d4420f18733f0d
Norbert Neudert <norbertneudert@gmail.com>
2020-01-08 13:12:05 +0100

    Updated DB  to work in new environment

---
+++ OpenMyGarageApi/OpenMyGarageApi/Data/Migrations/20200108115432_january first time setup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OpenMyGarageApi.Data.Migrations
+{
+    public partial class januaryfirsttimesetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
